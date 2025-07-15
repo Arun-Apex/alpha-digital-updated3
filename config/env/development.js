3571a5f99588cd0c04be77672618143c11c6a190
Arun-Apex <82267190+Arun-Apex@users.noreply.github.com>
2025-07-16 04:01:48 +0700

    Update development.js

--- config/env/development.js
+++ config/env/development.js
@@ -5,7 +5,7 @@ module.exports = {
     https: false,
     port: process.env.PORT || 3000,
     mongo: {
-        uri: 'mongodb://mongo/pisignage-server'
-
+       uri: process.env.MONGOLAB_URI ||
+            'mongodb://127.0.0.1:27017/pisignage-server-dev'
     }
 };
